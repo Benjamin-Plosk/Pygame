03d1374e5668c5a736f3b3ed93cc0d8dfac2a047
Hugodbr <62961034+Hugodbr@users.noreply.github.com>
2020-09-13 17:50:30 -0300

    constants

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+Global constants
+"""
+
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PURPLE = (255, 0, 255)
+GREEN_KEY = (0, 128, 128)
+
+# Screen dimensions
+SCREEN_WIDTH = 300
+SCREEN_HEIGHT = 300
